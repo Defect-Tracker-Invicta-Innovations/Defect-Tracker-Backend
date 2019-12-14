df87707979a828993682584cdb70d04c62cbae6a
DalistaaA <adalistaa@gmail.com>
2019-12-14 15:29:30 +0530

    finished License Type Apis

--- sgic-internal-services/product-service/src/main/java/com/sgic/internal/product/util/AppConstants.java
+++ sgic-internal-services/product-service/src/main/java/com/sgic/internal/product/util/AppConstants.java
@@ -2,4 +2,8 @@ package com.sgic.internal.product.util;
 
 public class AppConstants {
 
+	public static final String LICENSE_TYPE_URL = "/license-type";
+	public static final String LICENSE_TYPES_URL = "/license-types";
+	public static final String LICENSE_TYPE_URL_WITH_ID = "/license-type/{licenseTypeId}";
+
 }
