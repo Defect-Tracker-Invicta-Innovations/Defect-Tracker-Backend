c00d0c0c53ce337976db5612080fc3aa4a5a251e
DalistaaA <adalistaa@gmail.com>
2019-12-14 15:13:53 +0530

    Finished License Type Apis

---
+++ sgic-internal-services/product-service/src/main/java/com/sgic/internal/product/controller/dto/LicenseTypeDto.java
@@ -0,0 +1,24 @@
+package com.sgic.internal.product.controller.dto;
+
+public class LicenseTypeDto {
+
+	private Long id;
+	private String licenseType;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getLicenseType() {
+		return licenseType;
+	}
+
+	public void setLicenseType(String licenseType) {
+		this.licenseType = licenseType;
+	}
+
+}
